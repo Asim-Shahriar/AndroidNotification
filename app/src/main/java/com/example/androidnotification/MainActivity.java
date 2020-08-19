0fa7b6b050ff0fa23c0b6622a0fadad72bf32b47
Asim-Shahriar <asimshariar@gmail.com>
2020-08-20 01:53:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidnotification/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.androidnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Add onClick handlers to all the buttons.
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    private void sendNotification() {
+
+    }
+}
