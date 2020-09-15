a65731e7cb0f80f9569f180ce809a8029cd57029
Asim-Shahriar <asimshariar@gmail.com>
2020-09-16 02:57:03 +0600

    commit rest of the code of notification

--- app/src/main/java/com/example/androidnotification/MainActivity.java
+++ app/src/main/java/com/example/androidnotification/MainActivity.java
@@ -5,6 +5,13 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,15 +20,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String ACTION_UPDATE_NOTIFICATION =
+            "com.example.android.notifyme.ACTION_UPDATE_NOTIFICATION";
     private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
     private static final int NOTIFICATION_ID = 0;
+    private NotificationReceiver mReceiver = new NotificationReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Register the broadcast receiver to receive the update action from
+        // the notification.
+        registerReceiver(mReceiver,
+                new IntentFilter(ACTION_UPDATE_NOTIFICATION));
 
         // Add onClick handlers to all the buttons.
         button_notify = findViewById(R.id.notify);
@@ -31,12 +49,81 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+
+        button_update=findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update the notification.
+                updateNotification();
+            }
+        });
+
+        button_cancel=findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cancel the notification.
+                cancelNotification();
+            }
+        });
+
+        // Reset the button states. Enable only Notify button and disable
+        // update and cancel buttons.
+        setNotificationButtonState(true, false, false);
+
+    }
+
+    private void cancelNotification() {
+        // Cancel the notification.
+        mNotifyManager.cancel(NOTIFICATION_ID);
+
+        // Reset the buttons.
+        setNotificationButtonState(true, false, false);
+    }
+
+    private void updateNotification() {
+
+        // Load the drawable resource into the a bitmap image.
+        Bitmap androidImage = BitmapFactory
+                .decodeResource(getResources(), R.drawable.mascot_1);
+
+        // Build the notification with all of the parameters using helper
+        // method.
+        NotificationCompat.Builder notifyBuilder=getNotificationBuilder();
+        // Update the notification style to BigPictureStyle.
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                    .bigPicture(androidImage)
+                    .setBigContentTitle("Notification Updated!") );
+
+        // Deliver the notification.
+        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+
+        // Disable the update button, leaving only the cancel button enabled.
+        setNotificationButtonState(false, false, true);
+
     }
 
     private void sendNotification() {
+
+        // Sets up the pending intent to update the notification.
+        // Corresponds to a press of the Update Me! button.
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
       NotificationCompat.Builder notifyBuilder=getNotificationBuilder();
+
+        // Add the action button using the pending intent.
+      notifyBuilder.addAction(R.drawable.ic_update, "Update Notification", updatePendingIntent);
+
       mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+        // Enable the update and cancel buttons but disables the "Notify
+        // Me!" button.
+        setNotificationButtonState(false, true, true);
+
     }
+
     public void createNotificationChannel(){
         mNotifyManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
@@ -56,5 +143,43 @@ public class MainActivity extends AppCompatActivity {
                 .setContentText("This is your notification text.")
                 .setSmallIcon(R.drawable.ic_android);
         return notifyBuilder;
+    }
+
+    void setNotificationButtonState(Boolean isNotifyEnabled, Boolean
+            isUpdateEnabled, Boolean isCancelEnabled) {
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
+    }
+
+    /**
+     * The broadcast receiver class for notifications.
+     * Responds to the update notification pending intent action.
+     */
+    public class NotificationReceiver  extends BroadcastReceiver{
+
+        public NotificationReceiver(){
+
+        }
+        /**
+         * Receives the incoming broadcasts and responds accordingly.
+         *
+         * @param context Context of the app when the broadcast is received.
+         * @param intent The broadcast intent containing the action.
+         */
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // Update the notification.
+            updateNotification();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+
     }
 }
